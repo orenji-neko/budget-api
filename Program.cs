9bae3caa234baccc8819d2763d636e3493ecc968
orenji_neko <markanthony.enfermo@outlook.com>
2026-03-01 07:41:46 +0800

    feat: replace inmemory db provider with sqlite

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseInMemoryDatabase("dev")
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
 builder.Services.AddControllers();
 
