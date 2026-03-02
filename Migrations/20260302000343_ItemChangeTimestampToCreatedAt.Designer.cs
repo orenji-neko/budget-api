5bbe6f0bbb5bd0623450c28409b0e4e6c62e9d23
orenji_neko <markanthony.enfermo@outlook.com>
2026-03-02 08:06:20 +0800

    feat: add migration that changes timestamp to created at

--- Migrations/20260302000343_ItemChangeTimestampToCreatedAt.Designer.cs
+++ Migrations/20260302000343_ItemChangeTimestampToCreatedAt.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BudgetAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BudgetAPI.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260302000343_ItemChangeTimestampToCreatedAt")]
+    partial class ItemChangeTimestampToCreatedAt
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.3");
---
+++ Migrations/20260302000343_ItemChangeTimestampToCreatedAt.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BudgetAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class ItemChangeTimestampToCreatedAt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Timestamp",
+                table: "Items",
+                newName: "CreatedAt");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CreatedAt",
+                table: "Items",
+                newName: "Timestamp");
+        }
+    }
+}
